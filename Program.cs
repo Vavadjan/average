16d03965674168b3a68498c3477dbb0b0f7b9f21
Vavadjan <vavadjan@gmail.com>
2023-09-23 20:37:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вывести в консоль среднее арифметическое значение . 
+            //Стоп по колличеству 10 или по коду 9999.
+
+            int i = 0;
+            int sum = 0;
+            int number = 0;
+            int average = 0;
+
+            for (i = 0; i <= 9; i++)
+            {
+                Console.WriteLine("Введите число: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if (number != 9999)
+                {
+                    sum = sum + number; ;
+                }
+                else
+                {
+                    break;
+                }                
+            }
+
+            average = sum / i;
+
+            Console.WriteLine(i);
+        }
+    }
+}
